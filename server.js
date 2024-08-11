2fbebbdd9418130543285e194c678aac9aa394e3
Tushar <karansinghrajora1996@gmail.com>
2024-08-11 20:03:18 +0530

    Added GeminiAI model and Initialized it with API key

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const run = require("./controllers/geminiContoller");
 require("dotenv").config();
 const app = express();
 
 app.use(express.json());
 
-
+app.get("/", run);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server listening to port ${process.env.PORT}`);
